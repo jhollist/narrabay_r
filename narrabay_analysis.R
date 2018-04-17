44eca99a94b455e05d5b84ab6f9828029aa4067a
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-04-17 19:44:38 -0400

    script ready to roll

--- narrabay_analysis.R
+++ narrabay_analysis.R
@@ -9,9 +9,8 @@
 # <https://tidyverse.org> which is an opinionated (but effective) way to think 
 # about organizing and analyzing data in R.
 
-
 # checks for packages to make sure they are installed
-pkgs <- c("ggplot2", "RCurl", "dplyr", "tidyr", "readr", "plotly", "DT", "here",
+pkgs <- c("ggplot2", "RCurl", "dplyr", "tidyr", "readr", "plotly", "here",
           "stringr")
 for(i in pkgs){
   if(!i %in% installed.packages()){
@@ -21,60 +20,105 @@ for(i in pkgs){
 
 # Loads up packages from library (there are fancier ways, but this is explicit!)
 library(ggplot2)
-library(RCurl)
 library(dplyr)
 library(tidyr)
 library(readr)
 library(plotly)
-library(DT)
 library(lubridate)
 library(stringr)
 library(here)
 
 # Get data from Narragansett Bay Commissions snapshots 
+# Commented Out: Grab data live from snapshot.narrabay.com
+# Set starting and ending years 
+#year1 <- 2015
+#year2 <- 2017
+# Build URL to access data from the specified year range
+#date_url <- paste0(year1,"/01/01 12:00 AM-",year2,"/12/31 11:59 PM")
+#date_url_enc <- RCurl::curlEscape(date_url)
+#url <- paste0("http://snapshot.narrabay.com/app/Data/ExportCsv?page=1&p=Chl,pH,Temp&t=",
+#              date_url_enc, 
+#              "&l=1,2,3,4,5,6,7,8,9")
+#narrabay_data<-read_csv(url) 
 
-## Set starting and ending years 
-year1 <- 2017
-year2 <- 2018
+# Read in Data
+narrabay_data <- read_csv("narrabay_buoy_1517.csv") %>%
+  rename_all(tolower)
 
-## Build URL to access data from the specified year range
-date_url <- paste0(year1,"/01/01 12:00 AM-",year2,"/12/31 11:59 PM")
-date_url_enc <- curlEscape(date_url)
-url <- paste0("http://snapshot.narrabay.com/app/Data/ExportCsv?page=1&p=Chl,pH,Temp,Z&t=",
-              date_url_enc, 
-              "&l=5,9,6,7,8,1,2,3,4")
-narrabay_data<-read_csv(url)
+# Some basic QA
+range(narrabay_data$chl, na.rm = TRUE)
+range(narrabay_data$ph, na.rm = TRUE)
+range(narrabay_data$temp, na.rm = TRUE)
+
+## Problem #1 - -7999 looks a bit like a missing data value - should use NA
+### Get some more info on these values
+View(narrabay_data)
+
+narrabay_data_qa1 <- narrabay_data %>%
+  mutate(chl = case_when(chl == -7999 ~ NA_real_,
+                              TRUE ~ chl),
+         ph = case_when(ph == -7999 ~ NA_real_,
+                             TRUE ~ ph),
+         temp = case_when(temp == -7999 ~ NA_real_,
+                               TRUE ~ temp))
+
+### Check range again
+range(narrabay_data_qa1$chl, na.rm = TRUE)
+range(narrabay_data_qa1$ph, na.rm = TRUE)
+range(narrabay_data_qa1$temp, na.rm = TRUE)
+
+## Problem #2 EXTREMES!!!! 
+### Negative chl?
+### Negative pH?
+### HOT water
+narrabay_data_qa2 <- narrabay_data_qa1 %>%
+  filter(chl >= 0) %>%
+  filter(ph >= 0 & ph <= 14) %>%
+  filter(temp < 40)
+
+### Check ranges again
+range(narrabay_data_qa2$chl, na.rm = TRUE)
+range(narrabay_data_qa2$ph, na.rm = TRUE)
+range(narrabay_data_qa2$temp, na.rm = TRUE)
   
 
-# Manipulate Data
-
 ## Problem #1 - multiple variables in one column
-## Problem #2 - data encoding (and separating out)
-## Problem #3 - Might not actually need data every 15 minutes and at multiple
-## layers
+## Problem #2 - date encoding
+narrabay_tidy <- narrabay_data_qa2 %>%
+  mutate(layer = str_extract(.$location, "\\(\\w+\\)"),
+         location = str_extract(.$location, "\\w+")) %>%
+  mutate(layer = str_extract(.$layer, "\\w+")) %>%
+  mutate(date_time = mdy_hms(time)) %>%
+  mutate(date = date(date_time)) 
 
-narrabay_tidy <- narrabay_data %>%
-  rename_all(tolower) %>%
-  mutate(layer = location) %>%
-  mutate(location = str_extract(.$location, "\\(\\w+\\)")
-         
-         str_split(narrabay_data$location, " ",simplify = T)) 
+# Aggregate to daily and surface
+narrabay_analysis <- narrabay_tidy %>%
+  filter(layer == "surface") %>%
+  # Aggrgate to daily per location and per parameter
+  group_by(date, location) %>%
+  summarize(daily_chl = mean(chl),
+            daily_ph = mean(ph),
+            daily_temp = mean(temp))
 
-Let's tidy up this dataset.
+# Visualize Data
 
-```{r tidy}
-# separate location from layer
-# separate date and time
-# aggregate into hourly and surface:bottom average
-datatable(narrabay_data)
-```
+time_series_gg <- narrabay_analysis %>%
+  gather(parameter, measurement, daily_chl:daily_temp) %>%
+  ggplot(aes(x = date, y = measurement)) +
+  facet_grid(parameter~., scales = "free_y") +         
+  geom_point(aes(group = location)) +
+  theme_bw()
+time_series_gg  
 
-## Visualize Data
+chla_temp_gg <- narrabay_analysis %>%
+  ggplot(aes(x = daily_temp, y = daily_chl)) +
+  geom_point(aes(color = location, group = date)) +
+  geom_smooth(method = "lm", color = "black") +
+  facet_grid(location ~ .) +
+  theme_bw() +
+  scale_color_manual(values = c("darkblue", "darkred"))
+chla_temp_gg
 
-Next step is to visualize the data.  Let's look at the association between average temperature and chlorophyll.
-
-```{r plot_it, warning=FALSE, message=FALSE, eval=F}
-ggplotly()
-```
+ggplotly(chla_temp_gg)
 
 
